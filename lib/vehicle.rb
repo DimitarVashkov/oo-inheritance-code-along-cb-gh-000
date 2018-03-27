2835612ba1937371ff6dfb40f47133a0336404c2
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-27 20:59:03 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,13 @@
 class Vehicle
-
+  attr_accessor :wheel_size, :wheel_number
+  def initialize(w_size,number)
+    @wheel_size = w_size
+    @wheel_number = number
+  end
+  def go
+    'vrrrrrrrooom!'
+  end
+  def fill_up_tank
+    'filling up!'
+  end
 end
